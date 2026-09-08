a52e25320ef56a7ee490b5279115b5c2882a729b
Thế Ngô Thanh <222987@dmx.vn>
2026-09-08 21:03:30 +0700

    init project

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-slate-900 text-white">
+      <div className="p-8 bg-slate-800 rounded-2xl shadow-xl text-center border border-slate-700">
+        <h1 className="text-3xl font-bold text-sky-400 mb-2">
+          Vite + React + Tailwind CSS
+        </h1>
+        <p className="text-slate-300">
+          Tích hợp thành công! Bạn có thể bắt đầu sử dụng các class của Tailwind.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
