661cce906f111cf20de4e15c41dea60e32871e07
Thế Ngô Thanh <222987@dmx.vn>
2026-09-09 21:38:52 +0700

    config resolve path

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import './App.css'
+import { Header } from '@/components/layout/Header/Header'
+import { Footer } from '@/components/layout/Footer/Footer'
 
 function App() {
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-slate-900 text-white">
-      <div className="p-8 bg-slate-800 rounded-2xl shadow-xl text-center border border-slate-700">
-        <h1 className="text-3xl font-bold text-sky-400 mb-2">
-          Vite + React + Tailwind CSS
-        </h1>
-        <p className="text-slate-300">
-          Tích hợp thành công! Bạn có thể bắt đầu sử dụng các class của Tailwind.
-        </p>
-      </div>
+    <div>
+      <Header />
+      <Footer />
     </div>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +9,9 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(import.meta.dirname, './src'),
+    },
+  }
 })
